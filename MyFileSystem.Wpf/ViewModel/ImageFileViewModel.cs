afc358fd7321f424892bc38eaad22f70ee37a691
Vinge Nikolay <vn2756@gmail.com>
2022-02-24 20:08:28 +0400

    scale

--- MyFileSystem.Wpf/ViewModel/ImageFileViewModel.cs
+++ MyFileSystem.Wpf/ViewModel/ImageFileViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Windows.Media.Imaging;
 using MyFileSystem.Model;
 using MyFileSystem.Wpf.Model;
@@ -11,6 +12,11 @@ namespace MyFileSystem.Wpf.ViewModel
         private readonly IFileSystem _fileSystem;
         private bool _isActive;
         private BitmapFrame _imageSource;
+        private double _imageWidth;
+        private double _imageHeight;
+        private double _viewportWidth;
+        private double _viewportHeight;
+        private double _scale;
 
         public bool IsActive
         {
@@ -32,12 +38,70 @@ namespace MyFileSystem.Wpf.ViewModel
             }
         }
 
+        public double ImageWidth
+        {
+            get => _imageWidth;
+            set
+            {
+                _imageWidth = value;
+                RaisePropertyChanged(() => ImageWidth);
+            }
+        }
+
+        public double ImageHeight
+        {
+            get { return _imageHeight; }
+            set
+            {
+                _imageHeight = value;
+                RaisePropertyChanged(() => ImageHeight);
+            }
+        }
+
+        public double ViewportWidth
+        {
+            get { return _viewportWidth; }
+            set
+            {
+                _viewportWidth = value;
+                RaisePropertyChanged(() => ViewportWidth);
+            }
+        }
+
+        public double ViewportHeight
+        {
+            get { return _viewportHeight; }
+            set
+            {
+                _viewportHeight = value;
+                RaisePropertyChanged(() => ViewportHeight);
+            }
+        }
+
+        public double Scale
+        {
+            get => _scale;
+            set
+            {
+                _scale = value;
+                RaisePropertyChanged(() => Scale);
+            }
+        }
+
+        public ActionCommand ScaleIncCommand { get; }
+        public ActionCommand ScaleDecCommand { get; }
+        public ActionCommand FillImageCommand { get; }
+
         public ImageFileViewModel(
             IFileSystem fileSystem,
             IFileSystemItemSelectorManager fileSystemItemSelectorManager)
         {
             _fileSystem = fileSystem;
             fileSystemItemSelectorManager.ChangeSelectedItemEvent += OnChangeSelectedItemEvent;
+            Scale = 100.0;
+            ScaleIncCommand = new ActionCommand(ScaleInc);
+            ScaleDecCommand = new ActionCommand(ScaleDec);
+            FillImageCommand = new ActionCommand(FillImage);
         }
 
         private void OnChangeSelectedItemEvent(object sender, ChangeSelectedItemEventArgs e)
@@ -55,6 +119,40 @@ namespace MyFileSystem.Wpf.ViewModel
             using (var stream = new MemoryStream(contentBytes))
             {
                 ImageSource = BitmapFrame.Create(stream, BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
+                ApplyScale();
+            }
+        }
+
+        private void ApplyScale()
+        {
+            ImageWidth = _scale * ImageSource.Width / 100.0;
+            ImageHeight = _scale * ImageSource.Height / 100.0;
+        }
+
+        private void ScaleInc()
+        {
+            var newScale = _scale + 10.0;
+            if (newScale > 100.0) newScale = 100.0;
+            Scale = newScale;
+            ApplyScale();
+        }
+
+        private void ScaleDec()
+        {
+            var newScale = _scale - 10.0;
+            if (newScale < 10.0) newScale = 10.0;
+            Scale = newScale;
+            ApplyScale();
+        }
+
+        private void FillImage()
+        {
+            ImageWidth = ViewportWidth;
+            ImageHeight = ImageSource.Height * ViewportWidth / ImageSource.Width;
+            if (ImageHeight > ViewportHeight)
+            {
+                ImageWidth = ImageWidth * ViewportHeight / ImageHeight;
+                ImageHeight = ViewportHeight;
             }
         }
     }
